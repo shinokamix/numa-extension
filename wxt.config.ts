b0155e99b5b10ab0492c505cbae9a72f8ec586fb
Nail Abdullin <nail.dev@icloud.com>
2026-08-22 15:57:58 +0300

    chore: disable automatic browser launch

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   srcDir: 'src',
   modules: ['@wxt-dev/module-react'],
+  webExt: {
+    disabled: true,
+  },
   vite: () => ({
     plugins: [tailwindcss()],
   }),
